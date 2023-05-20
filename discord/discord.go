5b52ac4e0714db8079e848190d1fce874fce45f3
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-05-20 21:25:03 +0700

    initial empty discord instance

---
+++ discord/discord.go
@@ -0,0 +1,8 @@
+package discord
+
+import "github.com/bwmarrin/discordgo"
+
+func initial() {
+	botClient, err := discordgo.New("")
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import {
+	"github.com/tonkaew131/CsDiscordBot/discord"
+}
+
+func main() {
+	discord.initial()
+}
